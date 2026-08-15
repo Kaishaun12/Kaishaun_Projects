80a34e6b3597909be3d133131b4254c56ae378b0
Kaishaun12 <kaishaun2@gmail.com>
2026-08-14 21:47:57 -0400

    initial commit

---
+++ carbCalculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class CarbCalculator{
+	private:
+		int totalCarb_Meal;
+		double numUnits;
+	public:
+		CarbCalculator();
+		void calculateCarbs();
+};
+
+CarbCalculator::CarbCalculator(){
+	totalCarb_Meal = 0;
+	numUnits = 0.0;
+}
+
+void CarbCalculator::calculateCarbs(){
+	std::cout << "Please type in the number of carbs (in grams) of the food" << std::endl;
+	std::cin >> totalCarb_Meal;	
+}
+
+
+
+
+int main(){
+	CarbCalculator calc;
+
+	calc.calculateCarbs();
+
+	return 0;
+}
